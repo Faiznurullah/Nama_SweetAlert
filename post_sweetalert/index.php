2ce02143b4f8b8a185ec8e43c057d598d49858f4
Grakody <59213454+Faiznurullah@users.noreply.github.com>
2021-02-06 19:14:49 +0700

    Ubah Menjadi OOP

--- post_sweetalert/index.php
+++ post_sweetalert/index.php
@@ -1,3 +1,6 @@
+<?php
+include 'model.php';
+ ?>
 <!doctype html>
 <html lang="en">
   <head>
@@ -7,7 +10,7 @@
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
-
+    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
     <title>Sweet Alert</title>
   </head>
   <body>
@@ -42,28 +45,7 @@
 </div>
 
 <?php
- if(isset($_POST['kirim'])){
- $nama = htmlspecialchars($_POST['nama']);
-
-  echo " <script>
-  setTimeout(function (){
-
-  swal({
-     title: 'Good job!',
-     text: 'Nama Anda $nama',
-     icon: 'success',
-     button: 'Oke',
-   });
-
- },10);
-
-   </script>" ;
-
-
-
- }
-
-
+ $alert->alertin();
  ?>
 
 
@@ -72,7 +54,7 @@
 
     <!-- Option 1: Bootstrap Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
-    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+
     <!-- Option 2: Separate Popper and Bootstrap JS -->
     <!--
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js" integrity="sha384-q2kxQ16AaE6UbzuKqyBE9/u/KzioAlnx2maXQHiDX9d4/zp8Ok3f+M7DPm+Ib6IU" crossorigin="anonymous"></script>
